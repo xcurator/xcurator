8ac1f3a93af466adc6ee574e253a1f754581cbb8
Soheil Hassas Yeganeh <soheil@cs.toronto.edu>
2013-03-11 13:06:24 -0400

    Refactors the schema generator into a generic pipeline of mapping steps that can be configured at runtime. This commit contains only the basic schema extraction step as a proof of concept.

---
+++ src/edu/toronto/cs/xcurator/model/Attribute.java
@@ -0,0 +1,140 @@
+/*
+ *    Copyright (c) 2013, University of Toronto.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License"); you may
+ *    not use this file except in compliance with the License. You may obtain
+ *    a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *    License for the specific language governing permissions and limitations
+ *    under the License.
+ */
+package edu.toronto.cs.xcurator.model;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Represents an attribute in a schema.
+ *
+ * @author Soheil Hassas Yeganeh <soheil@cs.toronto.edu>
+ */
+public class Attribute {
+  public Attribute(Schema parent, String name, String path, boolean key) {
+    super();
+    this.name = name;
+    this.path = path;
+    this.parent = parent;
+    this.key = key;
+
+    instanceMap = new HashMap<SchemaInstance, Set<AttributeInstance>>();
+    reverseInstanceMap = new HashMap<String, Set<AttributeInstance>>();
+  }
+
+  public void addInstance(AttributeInstance instance) {
+    Set<AttributeInstance> attributes =
+        instanceMap.get(instance.schemaInstance);
+    if (attributes == null) {
+      attributes = new HashSet<AttributeInstance>();
+      instanceMap.put(instance.schemaInstance, attributes);
+    }
+    attributes.add(instance);
+
+    attributes = reverseInstanceMap.get(instance.content);
+    if (attributes == null) {
+      attributes = new HashSet<AttributeInstance>();
+      reverseInstanceMap.put(instance.content, attributes);
+    }
+    attributes.add(instance);
+  }
+
+  public boolean isOneToOne() {
+    for (Map.Entry<SchemaInstance, Set<AttributeInstance>> entry :
+        instanceMap.entrySet()) {
+      if (entry.getValue().size() > 1) {
+        return false;
+      }
+    }
+
+    for (Map.Entry<String, Set<AttributeInstance>> entry :
+        reverseInstanceMap.entrySet()) {
+      if (entry.getValue().size() > 1) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  public void setParent(Schema parent) {
+    this.parent = parent;
+  }
+
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getPath() {
+    return path;
+  }
+  public void setPath(String path) {
+    this.path = path;
+  }
+
+  public boolean isKey() {
+    return key;
+  }
+
+  public void setKey(boolean key) {
+    this.key = key;
+  }
+
+  public Schema getParent() {
+    return parent;
+  }
+
+  @Override
+  public String toString() {
+    return "A@ " + name;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof Attribute) {
+      Attribute attr = (Attribute) obj;
+      return attr.name.equals(this.name) && attr.parent.equals(this.parent) &&
+          attr.path.equals(this.path);
+    }
+    return super.equals(obj);
+  }
+
+  @Override
+  public int hashCode() {
+    return name.hashCode();
+  }
+
+  public Set<String> getTypeURIs() {
+    return typeURIs;
+  }
+
+  public void setTypeURIs(Set<String> typeURIs) {
+    this.typeURIs = typeURIs;
+  }
+
+  String name;
+  String path;
+  Schema parent;
+  boolean key;
+
+  Set<String> typeURIs = new HashSet<String>();
+  Map<SchemaInstance, Set<AttributeInstance>> instanceMap;
+  Map<String, Set<AttributeInstance>> reverseInstanceMap;
+}
---
+++ test/edu/toronto/cs/xcurator/generator/MappingGeneratorTest.java
@@ -0,0 +1,79 @@
+/*
+ *    Copyright (c) 2013, University of Toronto.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License"); you may
+ *    not use this file except in compliance with the License. You may obtain
+ *    a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *    License for the specific language governing permissions and limitations
+ *    under the License.
+ */
+package edu.toronto.cs.xcurator.generator;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import junit.framework.TestCase;
+
+import org.apache.xml.serialize.OutputFormat;
+import org.apache.xml.serialize.XMLSerializer;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import edu.toronto.cs.xcurator.generator.BasicSchemaExtraction;
+import edu.toronto.cs.xcurator.generator.MappingGenerator;
+import edu.toronto.cs.xml2rdf.utils.LogUtils;
+import edu.toronto.cs.xml2rdf.xml.XMLUtils;
+
+/**
+ * The main test case for xCurator v2.0.
+ *
+ * @author Soheil Hassas Yeganeh <soheil@cs.toronto.edu>
+ */
+public class MappingGeneratorTest extends TestCase {
+  public void testBasicStep() throws ParserConfigurationException, SAXException,
+      IOException {
+    LogUtils.shutup();
+
+    int[] max = new int[] { 10 }; // 10, 25, 50, 100, 250, 500, 1000 };
+
+    for (int m: max) {
+
+      System.out.println("\n\n  >> Running experiments for sample size: " + m +
+          " << \n\n");
+
+      PrintStream nout = new PrintStream("out.tmp");
+
+      Document rootDoc = XMLUtils.attributize(XMLUtils.parse(
+          MappingGeneratorTest.class.getResourceAsStream(
+              "/clinicaltrials/data/content.xml"), m));
+      OutputFormat format = new OutputFormat(rootDoc);
+      format.setLineWidth(65);
+      format.setIndenting(true);
+      format.setIndent(2);
+      XMLSerializer serializer = new XMLSerializer (
+          nout, format);
+          //System.out, format);
+      serializer.asDOMSerializer();
+      serializer.serialize(rootDoc);
+
+      Document doc = new MappingGenerator()
+          .addStep(new BasicSchemaExtraction(m))
+          .generateMapping(rootDoc.getDocumentElement(),
+              "http://www.linkedct.org/0.1#");
+
+      serializer = new XMLSerializer(
+          new FileOutputStream("output/new/output.ct.1." + m + ".xml"), format);
+      serializer.asDOMSerializer();
+      serializer.serialize(doc);
+    }
+  }
+}
