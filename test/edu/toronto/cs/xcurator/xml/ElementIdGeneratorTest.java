43585239614faf058ee5eb5349f4fd567e594aab
Eric Zhu <erkangzhu@gmail.com>
2014-02-24 16:55:42 -0500

    add test for element id generator

--- test/edu/toronto/cs/xcurator/xml/ElementIdGeneratorTest.java
+++ test/edu/toronto/cs/xcurator/xml/ElementIdGeneratorTest.java
@@ -16,6 +16,7 @@
 package edu.toronto.cs.xcurator.xml;
 
 import java.io.IOException;
+import java.rmi.activation.ActivationSystem;
 import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,8 +25,10 @@ import javax.xml.xpath.XPathExpressionException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
@@ -33,40 +36,74 @@ import org.xml.sax.SAXException;
  * @author ekzhu
  */
 public class ElementIdGeneratorTest {
-  
+
   private ElementIdGenerator idGenerator;
-  private final String idPattern = "http://edgar.sec.gov/Archives/edgar/data/1326801/000132680113000003/fb-20121231.xml#${UUID}";
+  private final String idPattern = "http://test/entity#${UUID}";
   private XPathFinder xpath;
   private final String examplePath = "/xbrli:xbrl/us-gaap:NonoperatingIncomeExpense";
+  private final String xbrli_identifier = "/xbrli:xbrl/xbrli:context/xbrli:entity/xbrli:identifier";
+  private final String xbrli_explicitMember = "/xbrli:xbrl/xbrli:context/xbrli:entity/xbrli:segment/xbrldi:explicitMember";
   private XmlParser parser;
   private Document dataDoc;
   private NsContext nsContext;
-  private Element testElement;
-  
+
   @Before
-  public void setup() {
-    try {
-      idGenerator = new ElementIdGenerator();
-      xpath = new XPathFinder();
-      parser = new XmlParser();
-      dataDoc = parser.parse(XPathFinderTest.class.getResourceAsStream(
-              "/secxbrls/data/fb-20121231.xml"), -1);
-      nsContext = new NsContext(dataDoc.getDocumentElement());
-      testElement = (Element) xpath.getNodesByPath(examplePath, null, dataDoc, nsContext)
-              .item(0);
-    } catch (SAXException | IOException | ParserConfigurationException | XPathExpressionException ex) {
-      Logger.getLogger(ElementIdGeneratorTest.class.getName()).log(Level.SEVERE, null, ex);
+  public void setup() throws SAXException, IOException, ParserConfigurationException {
+    idGenerator = new ElementIdGenerator();
+    xpath = new XPathFinder();
+    parser = new XmlParser();
+    dataDoc = parser.parse(XPathFinderTest.class.getResourceAsStream(
+            "/secxbrls/data/fb-20131231.xml"), -1);
+    nsContext = new NsContext(dataDoc.getDocumentElement());
+  }
+
+  @Test
+  /**
+   * The generator should generate the same ID for identical XML elements
+   */
+  public void test_generatedElementIdSameForIdenticalElements() throws XPathExpressionException, NoSuchAlgorithmException, IOException {
+    NodeList nl = xpath.getNodesByPath(xbrli_identifier, null, dataDoc, nsContext);
+    Element first = (Element) nl.item(0);
+    String id_first = idGenerator.generateId(idPattern, nsContext, first, dataDoc, xpath);
+    for (int i = 0; i < nl.getLength(); i++) {
+      String id = idGenerator.generateId(idPattern, nsContext, (Element) nl.item(i), dataDoc, xpath);
+      Assert.assertTrue(id_first.equals(id));
     }
   }
   
   @Test
-  public void test_generateElementId() {
-    try {
-      String id = idGenerator.generateId(idPattern, nsContext, testElement, dataDoc, xpath);
-      System.out.println("The generated ID is: " + id);
-      Assert.assertTrue(id.startsWith("http://edgar.sec.gov/Archives/edgar/data/1326801/000132680113000003/fb-20121231.xml#"));
-    } catch (NoSuchAlgorithmException | IOException | XPathExpressionException ex) {
-      Logger.getLogger(ElementIdGeneratorTest.class.getName()).log(Level.SEVERE, null, ex);
+  /**
+   * The generator should generate different IDs for different XML elements
+   */
+  public void test_generatedElementIdDifferentForDifferElements() throws XPathExpressionException, NoSuchAlgorithmException, IOException {
+    NodeList nl = xpath.getNodesByPath(xbrli_explicitMember, null, dataDoc, nsContext);
+    Element first = (Element) nl.item(0);
+    String id_first = idGenerator.generateId(idPattern, nsContext, first, dataDoc, xpath);
+    String dim_first = first.getAttribute("dimension");
+    String text_first = first.getTextContent().trim();
+    System.out.println(id_first);
+    System.out.println("The first one: \n" + first.toString().trim());
+    for (int i = 0; i < nl.getLength(); i++) {
+      Element e = (Element) nl.item(i);
+      String id = idGenerator.generateId(idPattern, nsContext, e, dataDoc, xpath);
+      if (e.getAttribute("dimension").equals(dim_first) &&
+              e.getTextContent().trim().equals(text_first)) {
+        System.out.println("This ID must be the same as the first: " + id);
+        System.out.println("The first one must be the same as this: \n" + e.toString().trim());
+        Assert.assertTrue(id_first.equals(id));
+      }
+      else {
+        Assert.assertTrue(!id_first.equals(id));
+      }
     }
+  }
+
+  @Test
+  public void test_generateElementId() throws XPathExpressionException, NoSuchAlgorithmException, IOException {
+    Element testElement = (Element) xpath.getNodesByPath(examplePath, null, dataDoc, nsContext)
+            .item(0);
+    String id = idGenerator.generateId(idPattern, nsContext, testElement, dataDoc, xpath);
+    System.out.println("The generated ID is: " + id);
+    Assert.assertTrue(id.startsWith("http://test/entity#"));
   }
 }
